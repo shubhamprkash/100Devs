e20a173880f4f5984a0705ef2c2ea8ab6544cb5d
ShubhamPrakash <d1901.bca@gmail.com>
2022-02-20 22:37:01 +0530

    played with js bg-changer

--- class12-materials/background-picker/js/main.js
+++ class12-materials/background-picker/js/main.js
@@ -1,19 +1,26 @@
 document.getElementById('purple').onclick = partyPurple
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
+document.getElementById('dark').onclick = goDark;
 
+let body = document.querySelector('body');
 
 function partyPurple() {
-  document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
-  document.querySelector('body').style.color = 'white'
+  body.style.backgroundColor = 'rgba(241,63,247,1)'
+  body.style.color = 'white'
 }
 
 function partyGreen() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
-  document.querySelector('body').style.color = 'white'
+  body.style.backgroundColor = 'rgba(0,253,81,1)'
+  body.style.color = 'white'
 }
 
 function partyBlue() {
-  document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
-  document.querySelector('body').style.color = 'white'
+  body.style.backgroundColor = 'rgba(0,254,255)'
+  body.style.color = 'white'
+}
+
+function goDark(){
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
 }
