d5368e811559ecaa8462468c4353dfdb51f53419
ShubhamPrakash <d1901.bca@gmail.com>
2022-02-25 04:13:07 +0530

    js Practice + cls 14 Add

--- class13-materials/temperature-converter/js/main.js
+++ class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,20 @@
 //Write your pseduo code first! 
+
+/*
+    read input value
+    convert from celsius to farehnight
+        inputValue * 9/5
+        +32
+       { (0°C × 9/5) + 32 = 32°F  }
+    print the output
+
+*/
+
+
+document.querySelector('#btn').addEventListener("click", convert);
+
+function convert(){
+    const inputVal = document.querySelector('#input').value;
+    let  result = (inputVal * (1.8)) + 32;
+    document.querySelector("#output").textContent=result;
+}
--- class14-materials/class-weekend-boring/js/main.js
+++ class14-materials/class-weekend-boring/js/main.js
@@ -5,7 +5,7 @@ document.querySelector('#check').addEventListener('click', check)
 function check() {
 
   const day = document.querySelector('#day').value
- 
+
   if(day === "tuesday" || day === "thursday"){
     console.log("YOU HAVE CLASS")
   }else if( day === "saturday" || day === "sunday"){
