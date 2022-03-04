c12b4d2c7ab5814ac3258a5411f55f7b75d28b5d
ShubhamPrakash <d1901.bca@gmail.com>
2022-03-04 08:12:55 +0530

    2 tasks done

--- class16-materials/sir-savage-the-21st/js/main.js
+++ class16-materials/sir-savage-the-21st/js/main.js
@@ -1,2 +1,12 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
 //Bonus can you make it print '21' 21 times to the dom?
+
+function looper(){
+    for(let i=1;i<22;i++)
+    {
+        console.log(i)
+        document.querySelector('#savageSays').innerText+=` \n ${i}`; 
+    }
+}
+
+looper();
--- class16-materials/stop-snacking/js/main.js
+++ class16-materials/stop-snacking/js/main.js
@@ -1,1 +1,12 @@
-//Create a function that grabs the number of snacks from the input and tells you to stop that many times
+//Create a function that grabs the number of snacks
+//  from the input and tells you to stop that many times
+
+document.querySelector('#help').addEventListener('click', stopSnacking)
+
+function stopSnacking(){
+    let numu = Number(document.querySelector('input').value);
+    document.querySelector('#stops').innerText =" ";
+        for(let i=1;i<= numu ;i++){
+            document.querySelector('#stops').innerText +=`  ${i} Stop!! \n`;
+        }
+}
