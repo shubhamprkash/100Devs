db8d88cd224cfe220fbe760d834c77eaa322a587
ShubhamPrakash <d1901.bca@gmail.com>
2022-03-04 08:31:47 +0530

    bringItOn part 1 done

--- class16-materials/bring-it-on-again/js/main.js
+++ class16-materials/bring-it-on-again/js/main.js
@@ -1,17 +1,45 @@
 // *Variables*
 // Declare a variable, assign it a value, and alert the value
 
+let val = 65;
+// alert(val);
+
 // Create a variable, divide it by 10, and console log the value
+
+var variable = 30;
+variable /= 3;
+console.log(variable); 
 
 // *Functions*
 // Create a function that multiplys 3 numbers and alerts the product
 
-// Create a function that takes in 4 numbers. Add the first two numbers and subtract the next two. Console log the result
+function multipier(a,b,c){
+    let reuslt = a *b * c;
+    alert( reuslt);
+}
+
+// Create a function that takes in 4 numbers. 
+// Add the first two numbers and subtract the next two. Console log the result
+
+function weird(a,b,c,d){
+    console.log();
+}
 
 // *Conditionals*
-// Create a function that takes in 3 numbers. Starting with 100 add the first number, subtract the second, and divide the third. If the value is greater then 25, console log "WE HAVE A WINNNA"
+// Create a function that takes in 3 numbers. 
+// Starting with 100 add the first number, subtract the second, 
+// and divide the third. If the value is greater then 25, 
+// console log "WE HAVE A WINNNA"
 
-// Create a function that takes in a day of the week. If it is a weekend alert, "weekend" and if not alert "week day". Handle capitilization and if the user does not enter a day of the week alert "Try again!"
+
+
+// Create a function that takes in a day of the week. If it is a weekend alert, 
+// "weekend" and if not alert "week day". 
+// Handle capitilization and if the user does not enter a day of the week 
+// alert "Try again!"
+
+
+
 
 //*Loops*
 //Create a function that takes in a number. Console log all values from 1 to that number or greater, but count by 3
--- class16-materials/bring-it-on/js/main.js
+++ class16-materials/bring-it-on/js/main.js
@@ -1,17 +1,67 @@
 // *Variables*
 // Create a variable and console log the value
 
+let val = 65;
+console.log(val);
+
 // Create a variable, add 10 to it, and alert the value
+
+let something = 10;
+something += 10; 
+// alert(something);
+
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
 
+function suntractor(a,b,c,d){
+    // alert(a-b-c-d);
+}
+
+suntractor(90,80,70,60);
+
 // Create a function that divides one number by another and returns the remainder
 
-// *Conditionals*
-// Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+function reminder(a,b){
+    return  a%b;
 
-// Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
+}
+
+console.log(reminder(10,3))
+
+// *Conditionals*
+// Create a function that adds two numbers and if the sum is 
+// greater than 50 alert Jumanji
+
+function adderJumanji(a,b){
+    let result= a+b;
+    if(result > 50){
+        alert("jumaji")
+    }
+}
+
+adderJumanji(20,10);
+
+
+// Create a function that multiplys three numbers and if the 
+// product is divisible by 3 alert ZEBRA
+
+function multiDiveder(a,b,c){
+    let pro = a*b*c;
+    if (pro % 3 === 0 ){
+        alert("Zebra")
+    }
+}
+
+multiDiveder(60,20,5);
 
 //*Loops*
-//Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
+//Create a function that takes in a word and a number. 
+// Console log the word x times where x was the number passed in
+
+function pataNahi(word,num){
+    for(let i = 1; i<=num ; i++){
+        console.log(word)
+    }
+}
+pataNahi('kuchBhi', 5);
